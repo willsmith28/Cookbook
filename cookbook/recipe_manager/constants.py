a00337770d93b69ccb80920ee247e335d4c75f09
Will Smith <william.smith1@ftr.com>
2020-03-31 15:12:39 -0500

    feat: add constants file for RecipeManager

---
+++ cookbook/recipe_manager/constants.py
@@ -0,0 +1,14 @@
+"""constants for RecipeManger
+"""
+REQUIRED_RECIPE_INGREDIENT_FIELDS = (
+    "amount",
+    "unit",
+    "specifier",
+    "name",
+    "recipe_id",
+)
+
+REQUIRED_STEP_FIELDS = (
+    "order",
+    "instruction",
+)
