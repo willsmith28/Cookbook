72d1ed8adc9d8ea9d94ec1af34d07c01147ccac8
Will Smith <william.smith1@ftr.com>
2020-04-09 15:07:44 -0500

    refactor: change all lists in django settings to tuples

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = "n-du=2-0keppq@^b@3xbpn&r4edh$18$19)0l%*pb4tyxav-+h"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ("localhost", "127.0.0.1")
 
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     "recipe_manager.apps.RecipeManagerConfig",
     "users.apps.UsersConfig",
     "django.contrib.admin",
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
-]
+)
 
-MIDDLEWARE = [
+MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -54,25 +54,26 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+)
 
 ROOT_URLCONF = "cookbook.urls"
 
-TEMPLATES = [
+TEMPLATES = (
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": (),
         "APP_DIRS": True,
         "OPTIONS": {
-            "context_processors": [
+            "context_processors": (
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
+            ),
         },
     },
-]
+)
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -107,14 +108,14 @@ AUTH_USER_MODEL = "users.User"
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = (
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
-]
+)
 
 
 # Internationalization
