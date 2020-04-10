de34c615fba1412189c9071c06815b18a9d696ea
Will Smith <william.smith1@ftr.com>
2020-04-10 15:00:40 -0500

    feat: add navigation bar and router content to app

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,56 @@
   <div id="app">
     <md-app>
       <md-app-toolbar class="md-primary">
+        <md-button
+          v-show="!menuVisible"
+          class="md-icon-button"
+          @click="toggleMenu"
+        >
+          <md-icon>menu</md-icon>
+        </md-button>
         <span class="md-title">Cookbook</span>
       </md-app-toolbar>
 
-      <md-app-drawer>
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
         <md-toolbar class="md-transparent" md-elevation="0">
           Navigation
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
         </md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>home</md-icon>
+            <span class="md-list-item-text">Recipes</span>
+          </md-list-item>
+        </md-list>
       </md-app-drawer>
+
+      <md-app-content>
+        <router-view />
+      </md-app-content>
     </md-app>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuVisible: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    }
+  }
+};
+</script>
 
 <style>
 #app {
