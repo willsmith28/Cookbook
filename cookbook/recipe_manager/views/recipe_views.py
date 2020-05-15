0e0caa51b6d0aed5638624eb7f4621e0f322ccf6
Will Smith <william.smith1@ftr.com>
2020-05-15 14:06:51 -0500

    feat: import serializers in recipe_views

--- cookbook/recipe_manager/views/recipe_views.py
+++ cookbook/recipe_manager/views/recipe_views.py
@@ -6,6 +6,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import status
+from ..serializers import (
+    RecipeSerializer,
+    IngredientInRecipeSerializer,
+    StepSerializer,
+    TagSerializer,
+)
 from .. import models, utils, constants
 
 
