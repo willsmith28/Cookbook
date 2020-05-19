96339e7b8586c5cafcefe9ecb539323a9ef25680
Will Smith <william.smith1@ftr.com>
2020-05-19 15:31:45 -0500

    feat: add component for listing ingredients in recipe

---
+++ client/src/components/IngredientsInRecipeList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(ingredient, ingredientIndex) in ingredients"
+        :key="ingredientIndex"
+      >
+        <span>
+          {{ ingredient.amount }}
+          {{ ingredient.unit }}
+          <ingredient-name-link :ingredient-id="ingredient.ingredient_id" />
+          {{ ingredient.specifier }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Fraction from "fraction.js";
+import IngredientNameLink from "@/components/IngredientNameLink";
+import { mapGetters } from "vuex";
+export default {
+  name: "IngredientsInRecipeList",
+  components: { IngredientNameLink },
+  props: {
+    recipeId: { type: [String, Number], required: true }
+  },
+  computed: {
+    ingredients() {
+      if (this.recipeId) {
+        const ingredients = this.getIngredientsInRecipe(this.recipeId).map(
+          ingredient => ({
+            ...ingredient,
+            amount: this.toFraction(ingredient.amount)
+          })
+        );
+        return ingredients;
+      } else {
+        return [];
+      }
+    },
+    ...mapGetters("recipe", ["getIngredientsInRecipe"])
+  },
+  methods: {
+    toFraction(number) {
+      return new Fraction(number).toFraction(true);
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  display: inline-block;
+  text-align: left;
+}
+div {
+  text-align: center;
+}
+</style>
