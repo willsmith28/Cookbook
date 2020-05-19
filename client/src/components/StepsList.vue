b3f450aa5702f7e04bd5729863767b7cb002680e
Will Smith <william.smith1@ftr.com>
2020-05-19 15:31:59 -0500

    feat: add component for listing steps in recipie

---
+++ client/src/components/StepsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="(instruction, stepIndex) of steps" :key="stepIndex">
+        <p>
+          {{ instruction }}
+        </p>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "StepsList",
+  props: { recipeId: { type: [String, Number], required: true } },
+  computed: {
+    steps() {
+      const steps = this.getSteps(this.recipeId);
+      return steps ? steps : [];
+    },
+    ...mapGetters("recipe", ["getSteps"])
+  }
+};
+</script>
+
+<style>
+ol {
+  display: inline-block;
+  text-align: left;
+}
+div {
+  text-align: center;
+}
+</style>
