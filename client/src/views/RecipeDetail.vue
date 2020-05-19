1367628b76a4ce0be03860470a04bbbe9d0bd0d1
Will Smith <william.smith1@ftr.com>
2020-05-19 15:32:33 -0500

    feat: use components in RecipeDetail page

--- client/src/views/RecipeDetail.vue
+++ client/src/views/RecipeDetail.vue
@@ -1,52 +1,23 @@
 <template>
-  <div>
-    <div class="md-layout">
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">{{ recipe.name }}</div>
-        </md-card-header>
+  <div v-if="!!recipe" class="md-layout md-alignment-top-center">
+    <recipe-overview-card :recipe-id="id" class="md-layout-item md-size-90" />
 
-        <md-card-content>
-          <p>{{ recipe.description }}</p>
-          <p>Cook Time: {{ recipe.cook_time }}</p>
-          <p>Number of servings: {{ recipe.servings }}</p>
-        </md-card-content>
-      </md-card>
+    <md-card
+      v-if="!!numberIngredientsInRecipe"
+      class="md-layout-item md-xsmall-size-90 md-size-45"
+    >
+      <md-card-header>
+        <div class="md-title">
+          {{ numberIngredientsInRecipe }} {{ pluralizeFormatIngredient }}
+        </div>
+      </md-card-header>
 
-      <md-card v-if="steps.length > 0" class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Steps</div>
-        </md-card-header>
-
-        <md-card-actions>
-          <md-card-expand-triggers>
-            <md-button class="md-icon-button">
-              <md-icon>keyboard_arrow_down</md-icon>
-            </md-button>
-          </md-card-expand-triggers>
-        </md-card-actions>
-
+      <md-card-expand v-if="numberIngredientsInRecipe > expandGreaterThan">
         <md-card-expand-content>
           <md-card-content>
-            <md-list>
-              <md-list-item
-                v-for="(instruction, stepIndex) of steps"
-                :key="stepIndex"
-              >
-                <div class="md-list-item-text">
-                  {{ stepIndex + 1 }} {{ instruction }}
-                </div>
-              </md-list-item>
-            </md-list>
+            <ingredients-in-recipe-list :recipe-id="id" />
           </md-card-content>
         </md-card-expand-content>
-      </md-card>
-
-      <md-card v-if="ingredients.length > 0" class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Ingredients</div>
-        </md-card-header>
-
         <md-card-actions>
           <md-card-expand-trigger>
             <md-button class="md-icon-button">
@@ -54,79 +25,99 @@
             </md-button>
           </md-card-expand-trigger>
         </md-card-actions>
+      </md-card-expand>
+      <md-card-content v-else>
+        <ingredients-in-recipe-list :recipe-id="id" />
+      </md-card-content>
+    </md-card>
+
+    <md-card
+      v-if="!!numberStepsInRecipe"
+      class="md-layout-item md-xsmall-size-90 md-size-45"
+    >
+      <md-card-header>
+        <div class="md-title">
+          {{ numberStepsInRecipe }} {{ pluralizeFormatStep }}
+        </div>
+      </md-card-header>
+
+      <md-card-expand v-if="numberStepsInRecipe > expandGreaterThan">
+        <md-card-actions>
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
+              <md-icon>keyboard_arrow_down</md-icon>
+              <md-tooltip md-direction="left" md-delay="300">
+                See detail
+              </md-tooltip>
+            </md-button>
+          </md-card-expand-trigger>
+        </md-card-actions>
 
         <md-card-expand-content>
           <md-card-content>
-            <md-list>
-              <md-list-item
-                v-for="(ingredient, ingredientIndex) in ingredients"
-                :key="ingredientIndex"
-              >
-                <div class="md-list-item-text">
-                  {{ toFraction(ingredient.amount) }}
-                  {{ ingredient.unit }}
-                  <ingredient-name-link
-                    :ingredient-id="ingredient.ingredient_id"
-                  />
-                  {{ ingredient.specifier }}
-                </div>
-              </md-list-item>
-            </md-list>
+            <steps-list :recipe-id="id" />
           </md-card-content>
         </md-card-expand-content>
-      </md-card>
-    </div>
+      </md-card-expand>
+
+      <md-card-content v-else>
+        <steps-list :recipe-id="id" />
+      </md-card-content>
+    </md-card>
   </div>
 </template>
 
 <script>
-import IngredientNameLink from "@/components/IngredientNameLink";
-import Fraction from "fraction.js";
+import RecipeOverviewCard from "@/components/RecipeOverviewCard";
+import IngredientsInRecipeList from "@/components/IngredientsInRecipeList";
+import StepsList from "@/components/StepsList.vue";
 import { mapActions, mapGetters } from "vuex";
 export default {
-  components: { IngredientNameLink },
+  components: { IngredientsInRecipeList, StepsList, RecipeOverviewCard },
   props: {
     id: {
       type: [Number, String],
       required: true
     }
   },
+  data: () => ({
+    expandGreaterThan: 11
+  }),
   computed: {
     recipe() {
       return this.getRecipe(this.id);
     },
-    ingredients() {
-      return this.getIngredientsInRecipe(this.id);
-    },
-    steps() {
-      return this.getSteps(this.id);
-    },
     tags() {
       return this.recipe.tags.map(tagID => this.getTag(tagID));
     },
+    numberStepsInRecipe() {
+      return this.stepInRecipeCount(this.id);
+    },
+    numberIngredientsInRecipe() {
+      return this.ingredientInRecipeCount(this.id);
+    },
+    pluralizeFormatIngredient() {
+      return `Ingredient${this.numberIngredientsInRecipe === 1 ? "" : "s"}`;
+    },
+    pluralizeFormatStep() {
+      return `Step${this.numberStepsInRecipe === 1 ? "" : "s"}`;
+    },
     ...mapGetters("recipe", [
-      "getIngredient",
       "getRecipe",
-      "getIngredientsInRecipe",
-      "getSteps",
+      "ingredientInRecipeCount",
+      "stepInRecipeCount",
       "getTag"
     ])
   },
-  created() {
-    Promise.all([
-      this.fetchIngredientsInRecipe(this.id),
-      this.fetchStepsInRecipe(this.id)
-    ]);
-  },
+
   methods: {
-    toFraction(number) {
-      return new Fraction(number).toFraction(true);
-    },
-    ...mapActions("recipe", [
-      "fetchIngredientsInRecipe",
-      "fetchStepsInRecipe",
-      "fetchRecipeDetail"
-    ])
+    ...mapActions("recipe", ["fetchRecipeDetail", "fetchTagDetail"])
   }
 };
 </script>
+
+<style scoped>
+.md-card {
+  margin: 4px;
+}
+</style>
