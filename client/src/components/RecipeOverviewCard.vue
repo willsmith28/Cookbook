57ece84a1bf0b544c6ca7fe96b71192fb574c061
Will Smith <william.smith1@ftr.com>
2020-05-19 15:31:26 -0500

    feat: make link only appear in RecipeOverviewCard if not on the details page

--- client/src/components/RecipeOverviewCard.vue
+++ client/src/components/RecipeOverviewCard.vue
@@ -1,51 +1,63 @@
 <template>
-  <router-link :to="{ name: 'recipe-detail', params: { id: recipe.id } }">
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <div class="md-title">{{ recipe.name }}</div>
-          <div class="md-subhead">
-            {{ formattedIngredientCount }} - {{ formattedStepCount }} -
-            {{ recipe.cook_time }} - serves {{ recipe.servings }}
-          </div>
-        </md-card-header>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">
+        {{ recipe.name }}
+      </div>
 
-        <md-card-content>
-          <p>
-            {{ recipe.description }}
-          </p>
-        </md-card-content>
-      </md-ripple>
-    </md-card>
-  </router-link>
+      <div class="md-subhead">
+        {{ formattedIngredientCount }} - {{ formattedStepCount }} -
+        {{ recipe.cook_time }} - serves {{ recipe.servings }}
+      </div>
+    </md-card-header>
+
+    <md-card-content>
+      <p>
+        {{ recipe.description }}
+      </p>
+
+      <div v-if="!!recipe.tags.length">
+        <tag-chips :tag-ids="recipe.tags" />
+      </div>
+    </md-card-content>
+
+    <md-card-actions v-if="$route.name !== 'recipe-detail'">
+      <router-link :to="`/recipe/${recipeId}/`">
+        See Details
+      </router-link>
+    </md-card-actions>
+  </md-card>
 </template>
 
 <script>
+import TagChips from "@/components/TagChips";
+import { mapGetters } from "vuex";
 export default {
+  name: "RecipeOverviewCard",
+  components: { TagChips },
   props: {
-    recipe: {
-      type: Object,
-      default: () => ({
-        id: null,
-        name: "",
-        description: "",
-        servings: null,
-        cook_time: "",
-        ingredients: [],
-        steps: [],
-        tags: []
-      })
+    recipeId: {
+      type: [Number, String],
+      required: true
     }
   },
   computed: {
+    recipe() {
+      return this.getRecipe(this.recipeId);
+    },
     formattedIngredientCount() {
-      const ingredientCount = this.recipe.ingredients.length;
+      const ingredientCount = this.ingredientInRecipeCount(this.recipeId);
       return `${ingredientCount} ingredient${ingredientCount > 1 ? "s" : ""}`;
     },
     formattedStepCount() {
-      const stepCount = this.recipe.steps.length;
+      const stepCount = this.stepInRecipeCount(this.recipeId);
       return `${stepCount} step${stepCount > 1 ? "s" : ""}`;
-    }
+    },
+    ...mapGetters("recipe", [
+      "ingredientInRecipeCount",
+      "stepInRecipeCount",
+      "getRecipe"
+    ])
   }
 };
 </script>
