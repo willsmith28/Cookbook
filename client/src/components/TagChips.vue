451b26fa5c29fe04583c52344bc1fabb8331b847
Will Smith <william.smith1@ftr.com>
2020-05-20 10:08:59 -0500

    feat: add component for tags chip

---
+++ client/src/components/TagChips.vue
@@ -0,0 +1,21 @@
+<template>
+  <md-chips v-model="tags" md-static />
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  props: {
+    tagIds: { type: Array, required: true }
+  },
+  computed: {
+    tags() {
+      return this.tagIds.map(tag_id => {
+        const tag = this.getTag(tag_id);
+        return `${tag.kind}: ${tag.value}`;
+      });
+    },
+    ...mapGetters("recipe", ["getTag"])
+  }
+};
+</script>
