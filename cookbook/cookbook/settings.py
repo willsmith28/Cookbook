d2b6d7bae6b26ef59759083117eeea2a3f8b2293
Will Smith <william.smith1@ftr.com>
2020-05-21 12:44:07 -0500

    feat: dockerize django app

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("APP_ENV", "DEBUG") == "DEBUG"
+DEBUG = os.environ.get("APP_ENV", "prod") == "dev"
 
-ALLOWED_HOSTS = ("localhost", "127.0.0.1")
+ALLOWED_HOSTS = ("localhost", "127.0.0.1", "0.0.0.0")
 
 
 # Application definition
