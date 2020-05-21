14ef4fcf8364579c23a711d1941ae2faecd17500
Will Smith <william.smith1@ftr.com>
2020-05-21 15:22:15 -0500

    feat: change settings to use environment variable

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("APP_ENV", "prod") == "dev"
 
-ALLOWED_HOSTS = ("localhost", "127.0.0.1", "0.0.0.0")
-
+ALLOWED_HOSTS = tuple(os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" "))
 
 # Application definition
 
@@ -115,8 +114,12 @@ WSGI_APPLICATION = "cookbook.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
