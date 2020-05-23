c97862c006b9e082d4f043a9b7f89096b4abee13
Will Smith <william.smith1@ftr.com>
2020-05-23 10:52:10 -0500

    fix: allow MHR in dev config for vue

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,1 +1,18 @@
-module.exports = {}
+module.exports = {
+  devServer: {
+    clientLogLevel: "debug",
+    disableHostCheck: true,
+    host: "0.0.0.0",
+    hot: true,
+    liveReload: true,
+    open: false,
+    overlay: true,
+    port: 8080,
+    public: "localhost:1337",
+    watchOptions: {
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+      poll: 1000
+    }
+  }
+};
