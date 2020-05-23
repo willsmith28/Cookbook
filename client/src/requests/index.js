6e8430b8e7e21272b2142e18ea8bfda3bf903821
Will Smith <william.smith1@ftr.com>
2020-05-23 11:11:12 -0500

    feat: pluralize recipe routes

--- client/src/requests/index.js
+++ client/src/requests/index.js
@@ -21,62 +21,62 @@ const requests = {
   },
 
   getAllIngredients() {
-    return client.get("/recipe-manager/ingredient/");
+    return client.get("/recipe-manager/ingredients/");
   },
 
   getIngredientUnits() {
-    return client.get("/recipe-manager/ingredient/units/");
+    return client.get("/recipe-manager/ingredients/units/");
   },
 
   createIngredient({ recipeId, ...ingredient }) {
-    return client.post("/recipe-manager/ingredient/", {
+    return client.post("/recipe-manager/ingredients/", {
       recipe_id: recipeId,
       ...ingredient
     });
   },
 
   getIngredient(id) {
-    return client.get(`/recipe-manager/ingredient/${id}/`);
+    return client.get(`/recipe-manager/ingredients/${id}/`);
   },
 
   getAllTags() {
-    return client.get("/recipe-manager/tag/");
+    return client.get("/recipe-manager/tags/");
   },
 
   createTag(tag) {
-    return client.post("/recipe-manager/tag/", tag);
+    return client.post("/recipe-manager/tags/", tag);
   },
 
   getTag(id) {
-    return client.get(`/recipe-manager/tag/${id}/`);
+    return client.get(`/recipe-manager/tags/${id}/`);
   },
 
   getTagKinds() {
-    return client.get("/recipe-manager/tag/kind/");
+    return client.get("/recipe-manager/tags/kind/");
   },
 
   getAllRecipes() {
-    return client.get("/recipe-manager/recipe/");
+    return client.get("/recipe-manager/recipes/");
   },
 
   createRecipe(recipe) {
-    return client.post("/recipe-manager/recipe/", recipe);
+    return client.post("/recipe-manager/recipes/", recipe);
   },
 
   getRecipe(id) {
-    return client.get(`/recipe-manager/recipe/${id}/`);
+    return client.get(`/recipe-manager/recipes/${id}/`);
   },
 
   editRecipe(recipe) {
-    return client.put(`/recipe-manager/recipe/${recipe.id}/`, recipe);
+    return client.put(`/recipe-manager/recipes/${recipe.id}/`, recipe);
   },
 
   getAllIngredientsInRecipe(recipeId) {
-    return client.get(`/recipe-manager/recipe/${recipeId}/ingredients/`);
+    return client.get(`/recipe-manager/recipes/${recipeId}/ingredients/`);
   },
 
   AddIngredientToRecipe(recipeId, { ingredientId, ...ingredientInRecipe }) {
-    return client.post(`/recipe-manager/recipe/${recipeId}/ingredients/`, {
+    return client.post(`/recipe-manager/recipes/${recipeId}/ingredients/`, {
       ingredient_id: ingredientId,
       ...ingredientInRecipe
     });
@@ -84,56 +84,56 @@ const requests = {
 
   getIngredientInRecipe(recipeId, ingredientId) {
     return client.get(
-      `/recipe-manager/recipe/${recipeId}/ingredients/${ingredientId}/`
+      `/recipe-manager/recipes/${recipeId}/ingredients/${ingredientId}/`
     );
   },
 
   editIngredientInRecipe(recipeId, { ingredientId, ...ingredientInRecipe }) {
     return client.put(
-      `/recipe-manager/recipe/${recipeId}/ingredients/${ingredientInRecipe.ingredientId}/`,
+      `/recipe-manager/recipes/${recipeId}/ingredients/${ingredientInRecipe.ingredientId}/`,
       { ingredient_id: ingredientId, ...ingredientInRecipe }
     );
   },
 
   removeIngredientFromRecipe(recipeId, ingredientId) {
     return client.delete(
-      `/recipe-manager/recipe/${recipeId}/ingredients/${ingredientId}/`
+      `/recipe-manager/recipes/${recipeId}/ingredients/${ingredientId}/`
     );
   },
 
   getAllStepsInRecipe(recipeId) {
-    return client.get(`/recipe-manager/recipe/${recipeId}/steps/`);
+    return client.get(`/recipe-manager/recipes/${recipeId}/steps/`);
   },
 
   addStepToRecipe(recipeId, step) {
-    return client.post(`/recipe-manager/recipe/${recipeId}/steps/`, step);
+    return client.post(`/recipe-manager/recipes/${recipeId}/steps/`, step);
   },
 
   getStepInRecipe(recipeId, order) {
-    return client.get(`/recipe-manager/recipe/${recipeId}/steps/${order}/`);
+    return client.get(`/recipe-manager/recipes/${recipeId}/steps/${order}/`);
   },
 
   editStepInRecipe(recipeId, step) {
     return client.put(
-      `/recipe-manager/recipe/${recipeId}/steps/${step.order}/`,
+      `/recipe-manager/recipes/${recipeId}/steps/${step.order}/`,
       step
     );
   },
 
   removeStepFromRecipe(recipeId, order) {
-    return client.delete(`/recipe-manager/recipe/${recipeId}/steps/${order}/`);
+    return client.delete(`/recipe-manager/recipes/${recipeId}/steps/${order}/`);
   },
 
   getAllTagsInRecipe(recipeId) {
-    return client.get(`/recipe-manager/recipe/${recipeId}/tags/`);
+    return client.get(`/recipe-manager/recipes/${recipeId}/tags/`);
   },
 
   addTagToRecipe(recipeId, tag) {
-    return client.post(`/recipe-manager/recipe/${recipeId}/tags/`, tag);
+    return client.post(`/recipe-manager/recipes/${recipeId}/tags/`, tag);
   },
 
   removeTagFromRecipe(recipeId, tagId) {
-    return client.delete(`/recipe-manager/recipe/${recipeId}/tags/${tagId}/`);
+    return client.delete(`/recipe-manager/recipes/${recipeId}/tags/${tagId}/`);
   }
 };
 
--- cookbook/recipe_manager/urls.py
+++ cookbook/recipe_manager/urls.py
@@ -4,39 +4,43 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ingredient/", views.IngredientView.as_view(), name="ingredient"),
-    path("ingredient/units/", views.ingredient_units, name="ingredient-units"),
+    path("ingredients/", views.IngredientView.as_view(), name="ingredient"),
+    path("ingredients/units/", views.ingredient_units, name="ingredient-units"),
     path(
-        "ingredient/<int:pk>/",
+        "ingredients/<int:pk>/",
         views.IngredientDetailView.as_view(),
         name="ingredient-detail",
     ),
-    path("tag/", views.TagView.as_view(), name="tag"),
-    path("tag/kind/", views.tag_kinds, name="tag_kind"),
-    path("tag/<int:pk>/", views.TagDetailView.as_view(), name="tag-detail"),
-    path("recipe/", views.RecipeView.as_view(), name="recipe"),
-    path("recipe/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe-detail"),
+    path("tags/", views.TagView.as_view(), name="tag"),
+    path("tags/kind/", views.tag_kinds, name="tag_kind"),
+    path("tags/<int:pk>/", views.TagDetailView.as_view(), name="tag-detail"),
+    path("recipes/", views.RecipeView.as_view(), name="recipe"),
+    path("recipes/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe-detail"),
     path(
-        "recipe/<int:recipe_pk>/ingredients/",
+        "recipes/<int:recipe_pk>/ingredients/",
         views.RecipeIngredient.as_view(),
         name="recipe-ingredients",
     ),
     path(
-        "recipe/<int:recipe_pk>/ingredients/<int:ingredient_pk>/",
+        "recipes/<int:recipe_pk>/ingredients/<int:ingredient_pk>/",
         views.RecipeIngredientDetail.as_view(),
         name="recipe-ingredient-detail",
     ),
     path(
-        "recipe/<int:recipe_pk>/steps/", views.RecipeStep.as_view(), name="recipe-steps"
+        "recipes/<int:recipe_pk>/steps/",
+        views.RecipeStep.as_view(),
+        name="recipe-steps",
     ),
     path(
-        "recipe/<int:recipe_pk>/steps/<int:order>/",
+        "recipes/<int:recipe_pk>/steps/<int:order>/",
         views.RecipeStepDetail.as_view(),
         name="recipe-step-detail",
     ),
-    path("recipe/<int:recipe_pk>/tags/", views.RecipeTag.as_view(), name="recipe-tags"),
     path(
-        "recipe/<int:recipe_pk>/tags/<int:tag_pk>/",
+        "recipes/<int:recipe_pk>/tags/", views.RecipeTag.as_view(), name="recipe-tags"
+    ),
+    path(
+        "recipes/<int:recipe_pk>/tags/<int:tag_pk>/",
         views.RecipeTagDelete.as_view(),
         name="recipe-tags-delete",
     ),
