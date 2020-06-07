17c3e1df4f0ecc079feb179a8d15168a6b9ffdb2
Will Smith <william.smith1@ftr.com>
2020-06-07 12:31:15 -0500

    feat: add fastapi docker setup

---
+++ fastapi/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$DATABASE" == "postgres" ]
+then
+    echo "waiting for postgres..."
+    while ! /bin/nc -z "$SQL_HOST" "$SQL_PORT"; do
+        sleep 0.5
+    done
+
+    echo "PostgreSQL started"
+fi
+
+alembic upgrade head
+
+exec "$@"
