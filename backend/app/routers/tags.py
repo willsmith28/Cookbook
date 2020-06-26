8a629ddcc4e358a1369ef2228041952c3520e138
Will Smith <william.smith1@ftr.com>
2020-06-26 13:04:06 -0500

    feat: use gino in tags router

--- backend/app/routers/tags.py
+++ backend/app/routers/tags.py
@@ -1,66 +1,80 @@
-# from typing import List
-# from sqlalchemy.sql.expression import exists
+from typing import List
+from asyncpg.exceptions import IntegrityConstraintViolationError
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from ..serializers import Tag, TagCreate, ErrorMessage
+from .. import models, utils
 
-# from fastapi.responses import JSONResponse
-# from ..database.serializers import Tag, TagCreate, ErrorMessage
-# from ..database import db
-
-# NOT_FOUND_RESPONSE = {"message": "tag not found"}
+NOT_FOUND_RESPONSE = {"message": "tag not found"}
 
 router = APIRouter()
 
-# #######
-# # /tags
-# #######
-# @router.get("/", tags=["tags"], response_model=List[Tag])
-# async def get_tags(database=Depends(db)):
-#     query = db.tags.select()
-#     return await database.fetch_all(query)
+#######
+# /tags
+#######
+@router.get("/", tags=["tags"], response_model=List[Tag])
+async def get_tags():
+    tags = await models.Tag.query.gino.all()
+    return tuple(tag.to_dict() for tag in tags)
 
 
-# @router.post("/", tags=["tags"], response_class=Tag)
-# async def create_tag(tag: TagCreate, database=Depends(db)):
-#     query = db.ingredients.insert().values(value=tag.value, kind=tag.kind)
-#     created_tag_id = await database.execute(query)
-#     return {**tag.dict(), "id": created_tag_id}
+@router.post(
+    "/",
+    tags=["tags"],
+    response_model=Tag,
+    responses={422: {"model": ErrorMessage}},
+    status_code=201,
+)
+async def create_tag(tag: TagCreate):
+    try:
+        created_tag = await models.Tag.create(**tag.dict())
+
+    except IntegrityConstraintViolationError as error:
+        response = JSONResponse(content={"message": str(error)}, status_code=422)
+
+    else:
+        response = created_tag.to_dict()
+
+    return response
 
 
-# #####################
-# # /tags/{tag_id: int}
-# #####################
-# @router.get(
-#     "/{tag_id}",
-#     tags=["tags"],
-#     response_model=Tag,
-#     responses={404: {"model": ErrorMessage}},
-# )
-# async def get_tag_detail(tag_id: int, database=Depends(db)):
-#     query = db.tags.select().where(db.tags.c.id == tag_id)
-#     tag = await database.fetch_one(query)
+#####################
+# /tags/{tag_id: str}
+#####################
+@router.get(
+    "/{tag_id}",
+    tags=["tags"],
+    response_model=Tag,
+    responses={404: {"model": ErrorMessage}},
+)
+async def get_tag_detail(tag_id: str):
+    tag = await models.Tag.get(tag_id)
+    return (
+        tag.to_dict()
+        if tag
+        else JSONResponse(status_code=404, content=NOT_FOUND_RESPONSE)
+    )
 
-#     return tag if tag else JSONResponse(status_code=404, content=NOT_FOUND_RESPONSE)
 
+@router.put(
+    "/{tag_id}",
+    tags=["tags"],
+    response_model=Tag,
+    responses={404: {"model": ErrorMessage}},
+)
+async def edit_tag(
+    tag_id: str, tag: TagCreate,
+):
+    if not (db_tag := await models.Tag.get(tag_id)):
+        return JSONResponse(status_code=404, content=NOT_FOUND_RESPONSE)
 
-# @router.put(
-#     "/{tag_id}",
-#     tags=["tags"],
-#     response_model=Tag,
-#     responses={404: {"model": ErrorMessage}},
-# )
-# async def edit_tag(
-#     tag_id: int, tag: TagCreate, database=Depends(db),
-# ):
-#     query = exists(db.tags.select().where(db.tags.c.id == tag_id))
-#     result = await database.execute(result)
-#     if result:
-#         query = (
-#             db.tags.update()
-#             .values(value=tag.value, kind=tag.kind)
-#             .where(db.tags.c.id == tag_id)
-#         )
-#         await database.execute(query)
+    try:
+        await db_tag.update(**tag.dict()).apply()
 
-#         return {**tag.dict(), "id": tag_id}
+    except IntegrityConstraintViolationError as error:
+        response = JSONResponse(content={"message": str(error)}, status_code=422)
 
-#     return JSONResponse(status_code=404, content=NOT_FOUND_RESPONSE)
+    else:
+        response = db_tag.to_dict()
+
+    return response
